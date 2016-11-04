ee065abd6bc766719dbaedf02835a10d5470f97c
patwong <p.wong8888@gmail.com>
2016-11-04 19:27:59 -0400

    commit #2

--- csharp_scrap/Program.cs
+++ csharp_scrap/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using csharp_scrap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace csharp_scrap
 {
     class Program
     {
+        /*
+        static void swapbyref(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        */
         static void Main(string[] args)
         {
             Console.WriteLine("program start-oh!");
@@ -17,7 +26,12 @@ namespace csharp_scrap
 
             Console.WriteLine("{0}, {1}, {2} years old", p.lastName, p.firstName, p.age);
             p.humanstats();
-            
+
+            int x = 1, y = 2;
+            Console.WriteLine("1: x is {0}, y is {0}", x, y);
+            AuxFuncs af1 = new AuxFuncs();
+            af1.swapbyref(ref x, ref y);
+            Console.WriteLine("2: x is {0}, y is {0}", x, y);
             Console.ReadLine();
         }
     }
