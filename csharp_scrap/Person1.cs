5f03406fa2ae37913135d2eb6d759bcf8452834e
Patrick <Patrick@PWPC2>
2016-11-04 18:25:45 -0400

    Add project files.

---
+++ csharp_scrap/Person1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_scrap
+{
+    class Person1
+    {
+        const double NumberOfLegs = 2;
+        const string Species = "Homo Sapien sapien";
+        static readonly DateTime SomeTime = DateTime.Now;
+        public string firstName;
+        public string lastName;
+        public int age;
+        public void humanstats()
+        {
+            Console.WriteLine("number of legs: {0}", NumberOfLegs);
+            Console.WriteLine("species: {0}", Species);
+            Console.WriteLine("datetime: {0}", SomeTime);
+        }
+    }
+
+}
---
+++ csharp_scrap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_scrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("program start-oh!");
+            Person1 p = new Person1();
+            p.firstName = "Bob";
+            p.lastName = "MacKenzie";
+
+            Console.WriteLine("{0}, {1}, {2} years old", p.lastName, p.firstName, p.age);
+            p.humanstats();
+
+
+            Console.ReadLine();
+        }
+    }
+}
